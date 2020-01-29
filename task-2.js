f7274caf21ad1184c308e7fd374a220fb8e44b84
IvanPustovit <pustovit.for@gmail.com>
2020-01-29 11:28:40 +0200

    Homework7_1

--- task-2.js
+++ task-2.js
@@ -1,80 +1,40 @@
+"use strict";
+
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
 
 const ingredients = [
-    'Картошка',
-    'Грибы',
-    'Чеснок',
-    'Помидоры',
-    'Зелень',
-    'Приправы',
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
 ];
 
-const listIngredients = document.createElement('ul')
-listIngredients.id = 'ingredients'
-const newList = document.createElement('li')
+const newListUl = document.querySelector("#ingredients");
 
-var totalSum = ingredients.reduce(function (sum, item) {
-    
-        newList.textContent = sum;
-        return item;
-    },listIngredients[0]);
+const listIngredients = function(arr) {
+  for (let el of arr) {
+    const newListLi = document.createElement("li");
+    newListLi.classList.add("item");
+    const listText = document.createTextNode(el);
+    newListLi.appendChild(listText);
+    newListUl.append(newListLi);
+  }
+  return newListUl;
+};
 
-// function getSums(arr) {
-//     const newList = document.createElement('li') 
-//     // if (!arr.length) return newList;
+// const listIngredients = function (arr) {
+//   const newListUl = document.createElement("ul");
+//   newListUl.id = "ingredients";
 
-//     var totalSum = arr.reduce(function (sum, item) {
-//         newList.textContent = sum;
-//         return sum;
-//     },listIngredients[0]);
-//     // totalSum;
-
-//     return totalSum;
+//   for (let el of arr) {
+//     const newListLi = document.createElement("li");
+//     const listText = document.createTextNode(el)
+//     newListLi.appendChild(listText)
+//     newListUl.append(newListLi)
+//   }
+//   return newListUl
 // }
 
-console.log(totalSum);
-
-
-// const addList = function(){
-//     const listIngredients = document.createElement('ul')
-//     listIngredients.id = 'ingredients'
-// console.log(listIngredients);
-
-//     const newList = document.createElement('li')
-//     for(i = 0; i < length; i = i + 1){
-
-//     }
-
-
-
-// }
-
-
-// console.log(addList(ingredients));
-
-
-// const listIngredients = document.createElement('ul')
-// listIngredients.id = 'ingredients'
-// const newList = document.createElement('li')
-
-// let c = ingredients.reduce()
-// let ele
-// for(let el of ingredients){
-//     ele = el
-// newList.textContent = el
-// listIngredients.appendChild(newList)
-// console.log(newList);
-// console.log(listIngredients);
-
-// console.log(el);
-// }
-//  console.log(ele);
-// // listIngredients.append(newList)
-// newList.textContent = (ingredients[ele])
-
-// const insertList = function (ingredients) {
-
-// }
-// console.log(listIngredients);
-// console.log(newList);
-// console.log(ingredients);
+console.log(listIngredients(ingredients));
