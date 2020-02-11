098617b48f7ba9e0c6e6f216ec7574ecb1d42165
IvanPustovit <pustovit.for@gmail.com>
2020-02-11 22:48:48 +0200

    Homework7_7

--- task-5.js
+++ task-5.js
@@ -1,20 +1,15 @@
-'use strict'
+"use strict";
 
 // Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
 // <input type="text" placeholder="Ваше имя?" id="name-input" />
 // <h1>Привет, <span id="name-output">незнакомец</span>!</h1>
 
-const nameInput = document.querySelector('#name-input')
-const nameOutput = document.querySelector('#name-output')
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
 function name(e) {
-    let nameOut = e.target.value ? nameOutput.textContent = e.target.value : nameOutput.textContent = 'незнакомец'
-
-    return nameOut
-
-    // nameInput.textContent = e.target.value ? nameOutput.textContent = e.target.value : nameOutput.textContent = 'незнакомец'
+  nameOutput.textContent = e.target.value ? e.target.value : "незнакомец";
 }
 
-
-nameInput.addEventListener('input', name)
+nameInput.addEventListener("input", name);
